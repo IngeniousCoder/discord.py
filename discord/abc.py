ded7e830dd4399d52ae417658ed3d861ff56b1ea
XtremeCoder <eltontay11@gmail.com>
2019-02-25 09:35:50 +0800

    Update abc.py

--- discord/abc.py
+++ discord/abc.py
@@ -740,7 +740,7 @@ class Messageable(metaclass=abc.ABCMeta):
 
         channel = await self._get_channel()
         state = self._state
-        content = str(content) if content is not None else None
+        content = str(content.replace('@', '@\u200b')) if content is not None else None
         if embed is not None:
             embed = embed.to_dict()
 
